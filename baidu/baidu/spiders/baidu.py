a08b9639963c3bdd556558cd802b7e064b43c158
Johnnnnnnnnny <41139822+Johnnnnnnnnny@users.noreply.github.com>
2018-07-21 15:52:19 +0800

    建立爬虫 生成json 取消抛出异常
    先前提醒输入参数数量的抛出异常有误
    被注释
    爬虫运行有误

--- baidu/baidu/spiders/baidu.py
+++ baidu/baidu/spiders/baidu.py
@@ -6,11 +6,12 @@ import sys
 class Baidu_Spider(scrapy.Spider):
 
     name = 'baidu_spider'
+    '''
     if len(sys.argv) < 2:
         raise Exception(' 老铁 你还没输入关键词 ！ ')
     if len(sys.argv) > 2:
         raise Exception(' 老铁 你的关键词超过一个了！')
-    
+    '''
     # 获取用户输入
     search = sys.argv[1]
     # 搜索后url
@@ -34,5 +35,6 @@ class Baidu_Spider(scrapy.Spider):
                 'introduction':result.css('div.c-abstract::text').extract_first()
                 
             }
+        # 下一页
         url = self.base_url + response.css('a::attr(href)').extract_first()
         yield scrapy.Request(url,callback=self.parse)
