415614718b906b550f8582a0053b44a71fed8a6e
Johnnnnnnnnny <41139822+Johnnnnnnnnny@users.noreply.github.com>
2018-07-21 15:39:39 +0800

    初步代码
    已有：标题 链接 介绍 下一页

---
+++ baidu.py
@@ -0,0 +1,38 @@
+import scrapy
+import sys
+
+
+
+class Baidu_Spider(scrapy.Spider):
+
+    name = 'baidu_spider'
+    if len(sys.argv) < 2:
+        raise Exception(' 老铁 你还没输入关键词 ！ ')
+    if len(sys.argv) > 2:
+        raise Exception(' 老铁 你的关键词超过一个了！')
+    
+    # 获取用户输入
+    search = sys.argv[1]
+    # 搜索后url
+    start_urls = [
+        'https://www.baidu.com/' + search
+    ] 
+    
+    base_url = 'https://www.baidu.com'
+
+    def parse(self,response):
+        result_lst = response.css('div.result c-container ')
+
+
+        for result in result_lst:
+            yield{
+                # 标题
+                'title':result.css('a.t::text').extract_first(),
+                # 链接
+                'url':result.css('a.t::attr(href)').extract_first(),
+                # 介绍
+                'introduction':result.css('div.c-abstract::text').extract_first()
+                
+            }
+        url = self.base_url + response.css('a::attr(href)').extract_first()
+        yield scrapy.Request(url,callback=self.parse)
