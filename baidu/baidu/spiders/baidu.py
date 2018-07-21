73e39a7d6878030110175586d8c5dcf0e77d8cef
Johnnnnnnnnny <41139822+Johnnnnnnnnny@users.noreply.github.com>
2018-07-21 16:53:04 +0800

    直接jizhi单页测试

--- baidu/baidu/spiders/baidu.py
+++ baidu/baidu/spiders/baidu.py
@@ -6,19 +6,34 @@ import sys
 class Baidu_Spider(scrapy.Spider):
 
     name = 'baidu_spider'
+    
+    # 输入参数抛出异常 目前有误
     '''
     if len(sys.argv) < 2:
         raise Exception(' 老铁 你还没输入关键词 ！ ')
     if len(sys.argv) > 2:
         raise Exception(' 老铁 你的关键词超过一个了！')
     '''
+
+    '''
+    可能有误
+
+
     # 获取用户输入
     search = sys.argv[1]
     # 搜索后url
     start_urls = [
-        'https://www.baidu.com/' + search
-    ] 
+        'https://www.baidu.com//s?wd=' + search
+    ]
     
+
+    '''
+
+    # 先以 jizhi 进行测试
+    start_urls = [
+        'https://www.baidu.com//s?wd=jizhi'
+    ]
+
     base_url = 'https://www.baidu.com'
 
     def parse(self,response):
@@ -28,13 +43,13 @@ class Baidu_Spider(scrapy.Spider):
         for result in result_lst:
             yield{
                 # 标题
-                'title':result.css('a.t::text').extract_first(),
+            #    'title':result.css('a.t::text').extract_first(),
                 # 链接
-                'url':result.css('a.t::attr(href)').extract_first(),
+            #    'url':result.css('a.t::attr(href)').extract_first(),
                 # 介绍
                 'introduction':result.css('div.c-abstract::text').extract_first()
                 
             }
         # 下一页
-        url = self.base_url + response.css('a::attr(href)').extract_first()
-        yield scrapy.Request(url,callback=self.parse)
+        #url = self.base_url + response.css('a::attr(href)').extract_first()
+        #yield scrapy.Request(url,callback=self.parse)
